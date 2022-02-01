ce8e103075874810ecc3928cf4b589accb895794
Joao Vitor Pereira da Silva <joaosilva@cijunsp.local>
2022-02-01 15:41:06 -0300

    ajuste bug nome joao dark theme

--- js/darktheme.js
+++ js/darktheme.js
@@ -1,18 +1,18 @@
 dktheme = document.getElementById("dktheme");
 estado = 0;
 
-dktheme.addEventListener("click",function (event) {
+dktheme.addEventListener("click", function(event) {
     event.preventDefault();
-    if (estado == 0){
+    if (estado == 0) {
         darktheme();
         estado = 1;
-    } else if (estado == 1){
+    } else if (estado == 1) {
         brighttheme();
         estado = 0;
     }
 });
 
-function darktheme(){
+function darktheme() {
     //header//
     document.querySelector("header").style.backgroundColor = "rgb(30,30,30)";
     document.querySelector("h2").style.color = "rgb(187,143,243)";
@@ -21,7 +21,7 @@ function darktheme(){
 
     //quem somos//
     document.getElementById("quem__somos").style.backgroundColor = "rgb(30,30,30)";
-    document.getElementById("h2quemsomos").style.color = "rgb(187,143,243)"; 
+    document.getElementById("h2quemsomos").style.color = "rgb(187,143,243)";
 
     //habilidades//
 
@@ -36,21 +36,21 @@ function darktheme(){
     document.getElementById("contactid").style.backgroundColor = "rgb(30,30,30)";
     document.getElementById("titulocontato").style.color = "rgb(187,143,243)";
     document.getElementById("gabcontato").style.color = "rgb(187,143,243)";
-    document.getElementById("jaocontato").style.color = "rgb(187,143,243)";
+    document.getElementById("joaocontato").style.color = "rgb(187,143,243)";
 
 
 }
 
-function brighttheme(){
+function brighttheme() {
     //header//
-    document.querySelector("header").style.backgroundColor = "white";   
+    document.querySelector("header").style.backgroundColor = "white";
     document.querySelector("h2").style.color = "#490a5c";
     as = document.querySelectorAll("a");
     for (var i = 0; i < 3; i++) as[i].style.color = "#490a5c";
 
     //quem somos//
     document.getElementById("quem__somos").style.backgroundColor = "white";
-    document.getElementById("h2quemsomos").style.color = "#490a5c"; 
+    document.getElementById("h2quemsomos").style.color = "#490a5c";
 
     //habilidades//
     document.getElementById("dif_single0").style.backgroundColor = "white";
@@ -64,7 +64,5 @@ function brighttheme(){
     document.getElementById("contactid").style.backgroundColor = "white";
     document.getElementById("titulocontato").style.color = "#490a5c";
     document.getElementById("gabcontato").style.color = "#490a5c";
-    document.getElementById("jaocontato").style.color = "#490a5c";
+    document.getElementById("joaocontato").style.color = "#490a5c";
 }
-
-
